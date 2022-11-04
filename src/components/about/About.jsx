8c1a94d84fd8516c89ff6bb744195e4b4fd73049
AbderrezakWEBDEV <razik.bouiche@gmail.com>
2022-11-04 21:04:49 +0100

    commit 2

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "../about/About.css";
 import ME from "../../assets/Photo.jpg";
-import { GiSkills } from "react-icons/gi";
 import { FaSignLanguage } from "react-icons/fa";
 import { SiSkillshare } from "react-icons/si";
 const About = () => {
@@ -11,59 +10,41 @@ const About = () => {
       <h2>A propos de moi</h2>
       <div className="container about-container">
         <div className="about_me">
-          <div className="about_me-image">
-            <img src={ME} alt="" className="image" />
-          </div>
-          <div>
-            <div classNme>
-              <p>
-                Mon aventure du developpement web est à son début, concevoir un
-                site web en respectant un cahier de charge, charte graphique ,
-                améliorer et apporter de nouvelles fonctionnalités d'un site web
-                et application, je suis disponible pour tous nouveaux challange.
-              </p>
-              <a href="#contact" value="contact" className="btn btn-primary">
-                Contactez-moi !
-              </a>
-            </div>
+          <div className="about_descrp">
+            <p>
+              Mon aventure du développement web est à son début. Concevoir,
+              améliorer et apporter de nouvelles user story d'un site web en
+              respectant un cahier de charge, charte graphique. Je suis
+              disponible pour tous nouveaux challange.
+            </p>
+            <a href="#contact" value="contact" className="btn btn-primary">
+              Contactez-moi !
+            </a>
           </div>
         </div>
         <div className="about_content">
-          <div className="about_cards">
-            <article className="about_card">
-              <GiSkills className="about_icon" />
-              <h5>Compétences Techniques</h5>
-              <h5>HTML5/CSS3 (Bootstrap)</h5>
-              <div className="niveau">80%</div>
-              <li>Javascript (Vuejs3, Reactjs, Angular)</li>
-              <div className="niveau">50%</div>
-              <li>Flutter/React Native</li>
-              <div className="niveau">40%</div>
-              <li>Wordpress,PHP8, SQL (laravel)</li>
-              <div className="niveau">50%</div>
-              <li>Caneva, Photoshop, Figma, Trello, UX/UI</li>
-              <div className="niveau">70%</div>
-              <li>Approche Agile 'Srum' </li>
-              <div className="niveau">50%</div>
-            </article>
-            <article className="about_card">
-              <FaSignLanguage className="about_icon" />
-              <h5>Langues</h5>
-              <li>Berbere 'Maternelle' </li>
-              <li>Français 'Courant'</li>
-              <li>Arabe'Courant' </li>
-              <li>Anglais 'Travail' </li>
-            </article>
-            <article className="about_card">
-              <SiSkillshare className="about_icon" />
-              <h5>Soft Stkills</h5>
-              <li> Exigent et travailleur</li>
-              <li>Rigoureux</li>
-              <li>Autonomie et Initiative</li>
-              <li>Capacité d’écoute</li>
-              <li>Force d’adaptation</li>
-            </article>
-          </div>
+          <article className="about_card">
+            <div className="about_me-image">
+              <img src={ME} alt="" className="image" />
+            </div>
+          </article>
+          <article className="about_card">
+            <FaSignLanguage className="about_icon" />
+            <h5>Langues</h5>
+            <li>Berbere 'Maternelle' </li>
+            <li>Français 'Courant'</li>
+            <li>Arabe'Courant' </li>
+            <li>Anglais 'Travail' </li>
+          </article>
+          <article className="about_card">
+            <SiSkillshare className="about_icon" />
+            <h5>Soft Stkills</h5>
+            <li> Exigent et travailleur</li>
+            <li>Rigoureux</li>
+            <li>Autonomie et Initiative</li>
+            <li>Capacité d’écoute</li>
+            <li>Force d’adaptation</li>
+          </article>
         </div>
       </div>
     </section>
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 import "../experience/experience.css";
 const Experience = () => {
-  return <section id="experience">experience</section>;
+  return (
+    <section id="experience">
+      <h5>Whats Skills I Have!</h5>
+      <h2>Formations & Compétences </h2>
+
+      <div className="abou_content">
+        <article className="about_card">
+          <h5>Formations</h5>
+          <li>Le Bocal Academy Nice & Marseille</li>
+          <li>02-2021|02-2022 Titre Développeur Web et Web Mobile (BAC+2)</li>
+          <li>ISEM Université Nice Sophia Antipolis</li>
+          <li>2013|2014 Master Economie internationale (BAC+5)</li>
+          <li>Université Nice Sophia Antipolis</li>
+          <li>2009|2010 Master Banque, Finance et Assurance (licence)</li>
+        </article>
+        <article className="about_card">
+          <h5>Compétences Front-end </h5>
+          <li>HTML5/CSS3 (Bootstrap)</li>
+          <div className="niveau">80%</div>
+          <li>Javascript (Vuejs3, Reactjs, Angular)</li>
+          <div className="niveau">50%</div>
+          <li>Flutter/React Native</li>
+          <div className="niveau">50%</div>
+        </article>
+        <article className="about_card">
+          <h5>Compétences Back-end</h5>
+          <li>Wordpress,PHP8, SQL (laravel)</li>
+          <div className="niveau">50%</div>
+          <li>Caneva, Photoshop, Figma, Trello, UX/UI</li>
+          <div className="niveau">70%</div>
+          <li>Approche Agile 'Srum' </li>
+          <div className="niveau">50%</div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
